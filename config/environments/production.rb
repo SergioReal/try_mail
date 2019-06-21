f7ef188593483bb0b23f30a27d58626e91786be4
SergioReal <52027314+SergioReal@users.noreply.github.com>
2019-06-21 17:38:04 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
+
 
   require 'action_mailer'
 
@@ -115,3 +115,4 @@ ActionMailer::Base.smtp_settings = {
 
    config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+end
