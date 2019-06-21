32fa2ee4f379042032edb662cde1677cf44eb2e4
SergioReal <52027314+SergioReal@users.noreply.github.com>
2019-06-21 16:48:54 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,14 +93,23 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
+  require 'action_mailer'
 
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+# config.action_mailer.default_url_options = { :host => 'localhost:3000', port: '3000' }
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.default :charset => "utf-8"
 ActionMailer::Base.smtp_settings = {
-:address              => "smtp.gmail.com",
-:port                 => "587",
-:domain               => 'gmail.com',
-:user_name            => 'arcasystememail@gmail.com',
-:password             => 'arcaalertemail4!',
-:authentication       => 'plain',
-:enable_starttls_auto => true  }
+  :address              => "smtp.gmail.com",
+  :port                 => "587",
+  :domain               => 'localhost:3000',
+  :user_name            => 'arcasystememail@gmail.com',
+  :password             => 'trytry444!',
+  :authentication       => 'login',
+  :enable_starttls_auto => true  }
+
+  config.action_mailer.perform_caching = false
+
+   config.action_mailer.default_url_options = { host: 'localhost:3000' }
